626dc9b34b6d0c880f678047ebde31627a8e86a0
rayshoo <fire@dragonz.dev>
2023-11-23 05:47:30 +0000

    [FIX] fix domain filters

--- 04_external_dns.tf
+++ 04_external_dns.tf
@@ -9,7 +9,7 @@ locals {
     ])
   ])
   external_dns_route53_zone_arns = data.aws_route53_zone.external_dns[*].arn
-  external_dns_domain_filters = "${join(",", [for s in var.external_dns_zones : format("%s", s)])}"
+  external_dns_domain_filters = "${join(",", [for s in var.external_dns_zones : format("%s", s.name)])}"
 }
 
 data "aws_route53_zone" "external_dns" {
